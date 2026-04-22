bf95c9033f182340c955611cd48ad6d6a62493db
Egor Kochetaev <61804964+EgoreTTa@users.noreply.github.com>
2026-04-22 18:40:58 +0500

    Create Beckman Coulter package with driver for model AU650

---
+++ Drivers/BeckmanCoulter/Access2.cs
@@ -0,0 +1,30 @@
+﻿namespace BeckmanCoulter
+{
+    using DriverBase;
+    using Infrastructure.DTOs.LIS;
+    using Serilog;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Access2 : IParser
+    {
+        public ILogger Logger { get; set; }
+        public Encoding Encoding { get; set; }
+        public void Clear()
+        {
+            throw new System.NotImplementedException();
+        }
+        public Task<ParserMessage> WriteAsync(byte[] bytes)
+        {
+            throw new System.NotImplementedException();
+        }
+        public Task<ParserMessage> ReadAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+        public Task<ParserMessage> WriteAsync(DeviceOrderDTO[] orders)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Drivers/BeckmanCoulter/DxH800.cs
@@ -0,0 +1,31 @@
+﻿namespace BeckmanCoulter
+{
+    using DriverBase;
+    using Infrastructure.DTOs.LIS;
+    using Serilog;
+    using System;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class DxH800 : IParser
+    {
+        public ILogger Logger { get; set; }
+        public Encoding Encoding { get; set; }
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ParserMessage> WriteAsync(byte[] bytes)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ParserMessage> ReadAsync()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ParserMessage> WriteAsync(DeviceOrderDTO[] orders)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
